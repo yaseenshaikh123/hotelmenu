a830a675aab865a5a587380cb194d2a99e5ca2c8
Yaseen <shaikhyaseen914@gmail.com>
2026-04-06 22:54:08 +0530

    added whitenoise

--- hotelmenu/settings.py
+++ hotelmenu/settings.py
@@ -115,9 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
