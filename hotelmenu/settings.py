67fc82c7226f7996753694a15f144720ce6cda32
Yaseen <shaikhyaseen914@gmail.com>
2026-04-12 10:49:28 +0530

    fix image loading

--- hotelmenu/settings.py
+++ hotelmenu/settings.py
@@ -122,6 +122,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- hotelmenu/urls.py
+++ hotelmenu/urls.py
@@ -5,3 +5,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('menu.urls')),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
